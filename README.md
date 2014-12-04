bf02029ec1d0277681218cae289c4e6fb92f2ce4
Craig Wickesser <codecraig@gmail.com>
2014-12-04 18:24:55 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# workflow
+
+Example usage:
+
+```
+# enable "spec" mode, all flags can start with two hyphens, such as --ms or --mode-spec
+workflow -ms
+workflow -ms=true
+workflow -mode-spec
+workflow -mode-spec=true
+```
---
+++ workflow.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+  "flag"
+  "fmt"
+)
+
+var specMode bool
+
+func init() {
+  const (
+    defaultSpecMode = false
+    usage = "enable spec mode"
+  )
+  flag.BoolVar(&specMode, "mode-spec", defaultSpecMode, usage)
+  flag.BoolVar(&specMode, "ms", defaultSpecMode, usage+" (shorthand)")
+}
+
+func main() {
+  flag.Parse()
+  fmt.Printf("specMode: %v", specMode)
+}
